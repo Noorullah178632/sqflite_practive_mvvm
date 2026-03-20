1d49f39b3f53d944c2ff062df63c98427655d29b
Noor Ullah <nooullah178632@gmail.com>
2026-03-20 15:51:26 +0500

    new data

---
+++ lib/view/practive_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PractiveView extends StatefulWidget {
+  const PractiveView({super.key});
+
+  @override
+  State<PractiveView> createState() => _PractiveViewState();
+}
+
+class _PractiveViewState extends State<PractiveView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
