7fe040f084843670f9ad5d5202777a17efcfa977
Noor Ullah <nooullah178632@gmail.com>
2026-03-24 14:19:59 +0500

    4

--- lib/view/practive_view.dart
+++ lib/view/practive_view.dart
@@ -35,6 +35,7 @@ class _PractiveViewState extends State<PractiveView> {
           Container(color: Colors.blue, child: Text("container 1")),
           Container(color: Colors.blue, child: Text("container 1")),
           Container(color: Colors.blue, child: Text("container 1")),
+          Container(color: Colors.blue, child: Text("container 1")),
         ],
       ),
     );
